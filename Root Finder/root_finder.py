feb3759c4970ad63c77212ef16d3c549c857153a
Carlos Neto <105734774+aka1Neto@users.noreply.github.com>
2023-11-30 00:08:10 +0000

    fixed newton_raphson function

--- Root Finder/root_finder.py
+++ Root Finder/root_finder.py
@@ -8,7 +8,7 @@ def input_function():
     
     try:
         function = lambdify(x, expression);
-        return function;
+        return (function, expression);
 
     except Exception as e:
         print("Error when creating function:", e);
@@ -24,8 +24,8 @@ def false_position(f, a, b):
     print(f"a={a}, f(a)={f(a):.4f}, b={b}, f(b)={f(b):.4f}, x={valor:.4f}, f(x)={f(valor):.4f}");
     return valor;
 
-def newton_raphson(f, xn):
-    der = diff(f(x), x);
+def newton_raphson(f, expr, xn):
+    der = diff(expr, x);
     der = lambdify(x, der);
     valor = xn - (f(xn) / der(xn));
     print(f"x={xn:.4f}, f(x)={f(xn):.4f}, f'(x)={der(xn):.4f}, xn={valor:.4f}, f(xn)={f(valor):.4f}");
@@ -36,7 +36,9 @@ def secant(f, a, b):
     print(f"Xn-1={a}, f(Xn-1)={f(a):.4f}, Xn={b}, f(Xn)={f(b):.4f}, Xn+1={valor:.4f}, f(Xn+1)={f(valor):.4f}");
     return valor;
 
-def get_root(method, f, a, b, e=1e-2):
+def get_root(method, function, a, b, e=1e-2):
+    f = function[0];
+    expr = function[1];
     iteration = 1;
     valor = None;
     while(1):
@@ -53,7 +55,7 @@ def get_root(method, f, a, b, e=1e-2):
             
             else:
                 valor = (a + b) / 2 if valor is None else valor;
-                valor = newton_raphson(f, valor);
+                valor = newton_raphson(f, expr, valor);
             
             
             if f(a) * f(valor) > 0:
@@ -71,11 +73,11 @@ def get_root(method, f, a, b, e=1e-2):
     
 
 def main():
-    function = input_function();
+    f = input_function();
     method = int(input("Which method?:\n1 - Bissection\n2 - False Position\n3 - Newton-Raphson\n4 - Secant\n"));
     a = float(input("Type the value of a: "));
     b = float(input("Type the value of b: "));
 
-    get_root(method, function, a, b);
+    get_root(method, f, a, b);
 
 main()
